598865552b669b8a43e76d2e0b6fa55f4f881408
zslx <1462240522@qq.com>
2024-01-14 20:11:36 +0800

    Day2

--- Day2/vite-project/tests/createElement.spec.js
+++ Day2/vite-project/tests/createElement.spec.js
@@ -4,18 +4,22 @@ import { it, expect, describe } from "vitest";
 describe("createElement", () => {
   it("should return vdom for element", () => {
     const el = React.createElement("div", null, "Hello React");
-    
-    expect(el).toEqual({
-      type:"div",
-      props:{
-        children:[{
-          type:"TEXT_ELEMENT",
-          props:{
-            nodeValue:"Hello React",
-            children:[]
-          }
-        }]
+
+    expect(el).toMatchInlineSnapshot(`
+      {
+        "props": {
+          "children": [
+            {
+              "props": {
+                "children": [],
+                "nodeValue": "Hello React",
+              },
+              "type": "TEXT_ELEMENT",
+            },
+          ],
+        },
+        "type": "div",
       }
-    })
+    `)
   });
 });
